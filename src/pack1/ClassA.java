db6e1463355dac9de355d950f192a88785558885
Mukhidd10 <100068987+Mukhidd10@users.noreply.github.com>
2022-09-14 17:01:39 +0530

    First commit...

--- src/pack1/ClassA.java
+++ src/pack1/ClassA.java
@@ -3,7 +3,14 @@ package pack1;
 public class ClassA {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String input = "Mukhidkhan";
+		String output = "";
+		
+		// Reverse string
+	for(int i=input.length()-1;i>=0;i--) 
+		
+			output = output+ input.charAt(i);
+			System.out.println(output);
 
 	}
 
